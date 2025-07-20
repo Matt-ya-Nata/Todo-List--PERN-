1732d85187bdb13de0c404931e802e226fca1630
Mateu <mathewsmaps@gmail.com>
2025-07-20 19:44:20 +0200

    Connecting Database and server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 const express = require("express")
 const app = express();
 const cors = require("cors");
+const pool = require("./db")
 
 // Middleware
 app.use(cors());
 
 //When getting data from a cleint side we get it from the request.body object 
 app.use(express.json());
+
+
+//Routes 
+
+//Create a todo
+
+//Get all todos
+
+//Get a todo
+
+//Update a todo
+
+//Delete a todo
+
 
 
 app.listen(5000,() =>{ 
