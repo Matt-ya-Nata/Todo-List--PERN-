f7b735570f365e11e6be8b878e5ab2934e1644e6
Mateu <mathewsmaps@gmail.com>
2025-07-21 22:03:32 +0200

    Building Routes with Postgres Quesries

--- server/db.js
+++ server/db.js
@@ -1,10 +1,11 @@
 const Pool = require("pg").Pool; 
-const pool =new Pool({
+
+const pool = new Pool({
      user:"postgres",
-     password:"kthl8822",
-     host: "localhost",
-     port:5433,
+     password:"",
+     host:"localhost",
+     port:5432,
      database:"perntodo"
-})
+});
 
 module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const cors = require("cors");
-const pool = require("./db")
+const pool = require("./db");
 
 // Middleware
 app.use(cors());
@@ -13,7 +13,24 @@ app.use(express.json());
 //Routes 
 
 //Create a todo
+app.post("/todos",async (req,res)=>{
 
+    try {
+
+        const {description} = req.body;
+        const newTodo = await pool.query("INSERT INTO todo(description) VALUES($1)",
+            [description]
+        
+        );
+
+        res.json(newTodo.rows[0])
+        
+    } catch (err) {
+        console.error(err.message);
+        
+    }
+
+});
 //Get all todos
 
 //Get a todo
