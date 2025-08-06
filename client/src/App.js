350813632293391f22790c08d40767d0561b6000
Matt-ya-Nata <109704700+Matt-ya-Nata@users.noreply.github.com>
2025-08-06 18:15:48 +0200

    Client Side setup (React)

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import { Fragment } from 'react/jsx-runtime';
+import React,Fragment from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment></Fragment>
+  );
+}
+
+export default App;
